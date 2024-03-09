5184f65ca326d4dde92033d8a6f619c99bdad708
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-03-09 22:08:29 +0530

    [ADDED]: MUSTAFA - Employee wage Computation Project setup

---
+++ src/com/mustafa/employeewagecom/EmpWageComputation.java
@@ -0,0 +1,13 @@
+package com.mustafa.employeewagecom;
+
+import java.util.Random;
+
+public class EmpWageComputation 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Employee wage Computation Program ");
+		
+	}
+}

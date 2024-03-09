4ac2050f17a8a0309b36da42aae2f514e3db603b
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-03-09 22:52:11 +0530

    [MODIFIED]: MUSTAFA - UC-2-Complete to Calculate Daily Employee Wage

--- src/com/mustafa/employeewagecom/EmpWageComputation.java
+++ src/com/mustafa/employeewagecom/EmpWageComputation.java
@@ -1,28 +1,30 @@
 package com.mustafa.employeewagecom;
 
-import java.util.*;
+import java.util.Random;
 
 public class EmpWageComputation 
 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcome to Employee wage Computation Program ");
+		System.out.println("Welcome to Employee wage Computation Program on Master Branch");
 		int attendance;
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("\nEnter the attendance: [1-Present / 0=Absent]:");
-		attendance = sc.nextInt();
+		int WAGE_PER_HOUR = 20;
+		int FULL_DAY_HOUR = 8;
+		int DAILY_EMP_WAGE = 0;
+
+		Random rm = new Random();
+		attendance = rm.nextInt(9)%2;
 		if(attendance == 1)
 		{
 			System.out.println("\nEmployee is present");
+			DAILY_EMP_WAGE = FULL_DAY_HOUR * WAGE_PER_HOUR;
+			System.out.println("Employee Wage =" + DAILY_EMP_WAGE);
+			
 		}
 		else 
 		{
 			System.out.println("\nEmployee is absent");
 		}
-
-		
 	}
 }

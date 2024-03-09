28f136b6e033d7d00436d9930b53bba15a7f5882
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-03-09 22:29:09 +0530

    [MODIFIED]:MUSTAFA - UC-1-Completed for Employee Attendance

--- src/com/mustafa/employeewagecom/EmpWageComputation.java
+++ src/com/mustafa/employeewagecom/EmpWageComputation.java
@@ -1,6 +1,6 @@
 package com.mustafa.employeewagecom;
 
-import java.util.Random;
+import java.util.*;
 
 public class EmpWageComputation 
 {
@@ -8,6 +8,21 @@ public class EmpWageComputation
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Employee wage Computation Program ");
+		int attendance;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("\nEnter the attendance: [1-Present / 0=Absent]:");
+		attendance = sc.nextInt();
+		if(attendance == 1)
+		{
+			System.out.println("\nEmployee is present");
+		}
+		else 
+		{
+			System.out.println("\nEmployee is absent");
+		}
+
 		
 	}
 }

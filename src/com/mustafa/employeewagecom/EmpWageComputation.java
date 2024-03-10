990834aefe22475a5888c639c3a044ffaf3c1cfc
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-03-10 14:47:35 +0530

    [MODIFIED]:MUSTAFA - UC-6-Completed for Calculate Working Hrs

--- src/com/mustafa/employeewagecom/EmpWageComputation.java
+++ src/com/mustafa/employeewagecom/EmpWageComputation.java
@@ -12,14 +12,17 @@ public class EmpWageComputation
 		int emp_type = 0;
 		int daily_wage = 0;
 		int total_wage = 0;
+		int total_working_hr = 0;
+		int total_working_days = 1;
 		final int WAGE_PER_HR = 20;
 		final int FULL_TIME_HR = 8;
 		final int PART_TIME_HR = 4;
 		final int WORK_DAYS = 20;
 	
-		for(int i = 1; i <= WORK_DAYS; i++)
+		while(total_working_hr <=100 && total_working_days <= 20)
 		{
-			System.out.println("Day:-"+i);
+			
+			System.out.println("Day:-"+ total_working_days);
 			Random rm = new Random();
 			attendance = rm.nextInt(9)%2;
 			
@@ -34,18 +37,23 @@ public class EmpWageComputation
 				case 0:
 					System.out.println("Employee is Full Timer");
 					daily_wage = FULL_TIME_HR * WAGE_PER_HR;
+					total_working_hr += FULL_TIME_HR;
 					break;
 				case 1:
 					System.out.println("Employee is Part Timer");
 					daily_wage = PART_TIME_HR * WAGE_PER_HR;
+					total_working_hr += PART_TIME_HR;
 					break;
 				}
 				total_wage += daily_wage;
 				System.out.println("Employee Wage =" + daily_wage);
 				break;
 			}
+			System.out.println("Total Working Hours:-"+total_working_hr);
 			System.out.println("----------------------");
-		}System.out.println("Monthly Wages:-"+total_wage);
+			total_working_days++;
+		}
+		System.out.println("Monthly Wages:-"+total_wage);
 		
 	}		
 }

85bdf2bd39ac73c7543ff2392671b680511a30e2
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-03-10 13:54:59 +0530

    [MODIFIED]:MUSTAFA - Refactore code for Calculate total Wage of Month

--- src/com/mustafa/employeewagecom/EmpWageComputation.java
+++ src/com/mustafa/employeewagecom/EmpWageComputation.java
@@ -11,6 +11,7 @@ public class EmpWageComputation
 		int attendance = 0;
 		int emp_type = 0;
 		int daily_wage = 0;
+		int total_wage = 0;
 		final int WAGE_PER_HR = 20;
 		final int FULL_TIME_HR = 8;
 		final int PART_TIME_HR = 4;
@@ -39,11 +40,12 @@ public class EmpWageComputation
 					daily_wage = PART_TIME_HR * WAGE_PER_HR;
 					break;
 				}
+				total_wage += daily_wage;
 				System.out.println("Employee Wage =" + daily_wage);
 				break;
 			}
 			System.out.println("----------------------");
-		}
+		}System.out.println("Monthly Wages:-"+total_wage);
 		
 	}		
 }

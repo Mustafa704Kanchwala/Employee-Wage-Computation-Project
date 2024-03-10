fbfe267696f970b0d66529f14daadc880aefbeaf
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-03-10 12:38:02 +0530

    [MODIFIED]:MUSTAFA - Refactored for Calculation of wage for Month

--- src/com/mustafa/employeewagecom/EmpWageComputation.java
+++ src/com/mustafa/employeewagecom/EmpWageComputation.java
@@ -14,31 +14,35 @@ public class EmpWageComputation
 		final int WAGE_PER_HR = 20;
 		final int FULL_TIME_HR = 8;
 		final int PART_TIME_HR = 4;
-		
-
-		Random rm = new Random();
-		attendance = rm.nextInt(9)%2;
-		
-		switch(attendance) {
-		case 0:
-			System.out.println("\nEmployee is absent");
-			break;
-		case 1:
-			System.out.println("\nEmployee is present");
-			emp_type = rm.nextInt(9)%2;
-			switch(emp_type) {
+		final int WORK_DAYS = 20;
+	
+		for(int i = 1; i <= WORK_DAYS; i++)
+		{
+			System.out.println("Day:-"+i);
+			Random rm = new Random();
+			attendance = rm.nextInt(9)%2;
+			
+			switch(attendance) {
 			case 0:
-				System.out.println("\nEmployee is Full Timer");
-				daily_wage = FULL_TIME_HR * WAGE_PER_HR;
+				System.out.println("Employee is absent");
 				break;
 			case 1:
-				System.out.println("\nEmployee is Part Timer");
-				daily_wage = PART_TIME_HR * WAGE_PER_HR;
+				System.out.println("Employee is present");
+				emp_type = rm.nextInt(9)%2;
+				switch(emp_type) {
+				case 0:
+					System.out.println("Employee is Full Timer");
+					daily_wage = FULL_TIME_HR * WAGE_PER_HR;
+					break;
+				case 1:
+					System.out.println("Employee is Part Timer");
+					daily_wage = PART_TIME_HR * WAGE_PER_HR;
+					break;
+				}
+				System.out.println("Employee Wage =" + daily_wage);
 				break;
 			}
-			System.out.println("Employee Wage =" + daily_wage);
-			break;
-		
+			System.out.println("----------------------");
 		}
 		
 	}		
